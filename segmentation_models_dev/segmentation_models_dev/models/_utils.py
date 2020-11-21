c887c9fcaf3762b01b0b3275994e89bee007237e
Reuben Wong <reuben.wong.ys@gmail.com>
2020-11-21 12:12:41 +0800

    modify freeze layers

--- segmentation_models_dev/segmentation_models_dev/models/_utils.py
+++ segmentation_models_dev/segmentation_models_dev/models/_utils.py
@@ -1,11 +1,12 @@
 from keras_applications import get_submodules_from_kwargs
+import tensorflow_addons as tfa
 
 
 def freeze_model(model, **kwargs):
     """Set all layers non trainable, excluding BatchNormalization layers"""
     _, layers, _, _ = get_submodules_from_kwargs(kwargs)
     for layer in model.layers:
-        if not isinstance(layer, layers.BatchNormalization):
+        if not isinstance(layer, layers.BatchNormalization) or not isinstance(layer, tfa.layers.GroupNormalization):
             layer.trainable = False
     return
 
